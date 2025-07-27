31abc8ff164721358955577e27cf698b0e54f0ba
Hillary Okoth <hillaryokoth002@gmail.com>
2025-07-28 00:48:41 +0300

    Django calculator application

--- calculator_app/admin.py
+++ calculator_app/admin.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from .models import CalculationHistory, CalculatorConstant
+from .models import CalculationHistory
 
 @admin.register(CalculationHistory)
 class CalculationHistoryAdmin(admin.ModelAdmin):
     list_display = ('expression', 'result', 'created_at', 'user')
     search_fields = ('expression', 'result')
     list_filter = ('created_at',)
-
-@admin.register(CalculatorConstant)
-class CalculatorConstantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'value', 'scientific_value')
-    search_fields = ('name', 'description')
